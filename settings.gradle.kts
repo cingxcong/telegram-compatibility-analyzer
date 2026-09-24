37572f8c29ad7d74b9120d2d20b5403b858d250e
CingCOng <cingxcong@proton.me>
2026-09-24 13:47:01 +0530

    chore: bootstrap analyzer scaffold

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "telegram-compatibility-analyzer"
+include(":analyzer")
